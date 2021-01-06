dd17d12a0cb6055e8a2a2646468ab1ad8433420c
Cristian <crlorenzo777@gmail.com>
2021-01-06 23:23:20 +0100

    quick click

---
+++ 05_quick_click/Assets/_Scripts/Launch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    public float forceMaxValue = 14f;
+    public float forceMinValue = 10f;
+    public float maxTorque = 10f;
+    public float minTorque = -10f;
+    public float rotationSpeed = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+
+        LaunchTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 rotation = rotationSpeed * new Vector3(1f, 1f, 0f);
+        //transform.Rotate(rotation * Time.deltaTime);
+    }
+
+    ///<summary>Aplica al gameObject una fuerza y torque de lanzamiento</summary>
+    void LaunchTarget()
+    {
+        float forceValue = Random.Range(forceMinValue, forceMaxValue);
+        float torqueValue = Random.Range(minTorque, maxTorque);
+
+        _rigidbody.AddForce(forceValue * transform.up, ForceMode.Impulse);
+        _rigidbody.AddTorque(torqueValue * Vector3.one, ForceMode.Impulse);
+    }
+}
