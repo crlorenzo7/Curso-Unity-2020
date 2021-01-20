749e69e90ffc77a822cba00a82c5ff41bb437a29
Cristian <crlorenzo777@gmail.com>
2021-01-20 01:01:09 +0100

    john_lemon

---
+++ 06_John_lemon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float rotationSpeed = 20f;
+    Rigidbody _rigidbody;
+    Animator _animator;
+
+    Vector3 movement;
+    Quaternion rotation = Quaternion.identity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        bool existsMovement = vertical != 0 || horizontal != 0;
+
+        _animator.SetBool("isWalking", existsMovement);
+
+        movement.Set(horizontal, 0f, vertical);
+        movement.Normalize();
+
+        Vector3 newForward = Vector3.RotateTowards(transform.forward, movement, rotationSpeed * Time.fixedDeltaTime, 0f);
+        rotation = Quaternion.LookRotation(newForward);
+
+
+    }
+
+    private void OnAnimatorMove()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + movement * _animator.deltaPosition.magnitude);
+        _rigidbody.MoveRotation(rotation);
+    }
+
+}
